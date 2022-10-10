e79288a772a786601dc32711c24518b0cbc498d2
ashfoste <ashton.foster@du.edu>
2022-10-10 16:03:08 -0600

    Lab 6 completed

--- Labs/Assets/Lab 6/HUDController.cs
+++ Labs/Assets/Lab 6/HUDController.cs
@@ -2,53 +2,129 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UIElements;
 
 public class HUDController : MonoBehaviour
 {
 
     [SerializeField] private UIDocument UIDoc;
-    [SerializeField] private float LevelTime;
+    [SerializeField] private float timeLeft;
+    private float lastTimeLeft;
     [SerializeField] Sprite HeartImage;
-    [SerializeField] private int NumLives=3;
+    [SerializeField] private int NumLives = 3;
+    [SerializeField] PlayerInput playerInput;
     // refrence to the hearts container to add a heart to
     private VisualElement heartsContainer;
+    private VisualElement root;
 
     private Label timeLabel;
+
+    private bool paused = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
-        VisualElement root = UIDoc.rootVisualElement;
+        root = UIDoc.rootVisualElement;
 
         heartsContainer = root.Q<VisualElement>("Lives");
 
         UpdateHearts(heartsContainer);
+
+
+
     }
     // life up nad life down exist in case anything else wants to add or remove a single life
-    private void LifeUp() {
+
+    private void Awake()
+    {
+        // the Q function is for query, so we look in the tree for the 
+        // element which is called "time-left", i.e., our time left label
+        timeLabel = UIDoc.rootVisualElement.Q<Label>("time-left");
+
+        // now set it to be the string representation of how much time is left
+        timeLabel.text = ((int)timeLeft).ToString();
+
+        // mark the last time we set this
+        lastTimeLeft = timeLeft;
+    }
+
+    private void LifeUp()
+    {
         NumLives++;
         UpdateHearts(heartsContainer);
     }
-    private void LifeDown() { 
+    private void LifeDown()
+    {
         NumLives--;
         UpdateHearts(heartsContainer);
     }
     // these dont work for some reason, idk, i have tried everything i can tink of. i know it can read the keys beigh pressed because the other one works
     //i just dont know whare the disconeect i appening in unity with the input actions
-    public void AddLife(InputAction.CallbackContext context) {
-        LifeUp();
+    public void AddLife(InputAction.CallbackContext context)
+    {
         if (context.performed)
         {
             Debug.Log("go up");
+            LifeUp();
         }
     }
-    public void RemoveLife(InputAction.CallbackContext context) {
-        LifeDown();
+    public void RemoveLife(InputAction.CallbackContext context)
+    {
         if (context.performed)
         {
             Debug.Log("go down");
+            LifeDown();
+        }
+    }
+
+
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            SwitchActionMap(playerInput, "UI");
+            // make the ui visible
+            //root.style.visibility = Visibility.Visible;
+            root.style.visibility = Visibility.Hidden;
+            paused = true;
+        }
+    }
+
+
+    public void ButtonPlay()
+    {
+        SwitchActionMap(playerInput, "Player");
+        root.style.visibility = Visibility.Visible;
+        paused = false;
+    }
+    public void Play(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            SwitchActionMap(playerInput, "Player");
+            root.style.visibility = Visibility.Visible;
+            paused = false;
+        }
+    }
+
+    private void SwitchActionMap(PlayerInput playerInput, string mapName)
+    {
+        playerInput.currentActionMap.Disable();
+        playerInput.SwitchCurrentActionMap(mapName);
+
+        switch (mapName)
+        {
+            case "UI":
+                UnityEngine.Cursor.visible = true;
+                UnityEngine.Cursor.lockState = CursorLockMode.None;
+                break;
+            default:
+                UnityEngine.Cursor.visible = false;
+                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                break;
         }
     }
 
@@ -65,14 +141,14 @@ public class HUDController : MonoBehaviour
             return;
         }
 
-                while (numCurrent > NumLives)
-                {
+        while (numCurrent > NumLives)
+        {
 
-                    heartsContainer.RemoveAt(numCurrent-1);
-                    numCurrent = HeartsContainer.childCount;
+            heartsContainer.RemoveAt(numCurrent - 1);
+            numCurrent = HeartsContainer.childCount;
 
-                }
-         while (numCurrent < NumLives)
+        }
+        while (numCurrent < NumLives)
         {
             Image heart = new Image();
             heart.sprite = HeartImage;
@@ -87,23 +163,23 @@ public class HUDController : MonoBehaviour
             heartsContainer.Add(heart);
             numCurrent = HeartsContainer.childCount;
         }
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Keyboard kb = Keyboard.current;
+        if (!paused)
+        {
+            // now subtract how much time has elapsed
+            timeLeft = Mathf.Max(0, timeLeft - Time.deltaTime);
 
-        if (kb.digit1Key.wasPressedThisFrame)
-        {
-            LifeUp();
-            //Debug.Log("heh");
-        }
-        if (kb.digit2Key.wasPressedThisFrame)
-        {
-            LifeDown();
-            //Debug.Log("kek");
+            // update only about each second please        
+            if (lastTimeLeft - timeLeft >= .9)
+            {
+                timeLabel.text = ((int)timeLeft).ToString();
+                lastTimeLeft = timeLeft;
+            }
         }
     }
 }
