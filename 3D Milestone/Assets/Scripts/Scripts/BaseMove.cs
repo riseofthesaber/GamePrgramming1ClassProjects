967e50a28d1bd68ec471e4ec6e9ee535a80c34f7
ashfoste <ashton.foster@du.edu>
2022-10-30 15:48:37 -0600

    3D milestone almost completed

---
+++ 3D Milestone/Assets/Scripts/Scripts/BaseMove.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMove : MonoBehaviour
+{
+
+    virtual public void Move(Vector3 moveDir)
+    {
+        throw new System.NotImplementedException();
+    }
+    virtual public void RotateCharacter()
+    {
+        throw new System.NotImplementedException();
+    }
+    virtual public void Jump()
+    {
+        throw new System.NotImplementedException();
+    }
+    virtual public void JumpCanceled()
+    {
+        throw new System.NotImplementedException();
+    }
+    virtual public void Dance()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    virtual protected void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    virtual protected void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    virtual protected void Update()
+    {
+
+    }
+
+    virtual protected void FixedUpdate()
+    {
+
+    }
+}
+
